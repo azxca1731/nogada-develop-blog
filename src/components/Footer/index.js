205f86b88426efec4df97893e4642f6718574bfa
azxca1731 <azxca1731@gmail.com>
2018-11-17 14:39:08 +0900

    테마 적용

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -5,15 +5,22 @@ import './style.scss'
 const Footer = ({ author, title }) => (
   <div className="footer">
     <div className="container">
-      <hr className="border-primary" />
-      <p>
-        {title}
-        <Link to="/profile/">
-          <br />
-          <strong>{author}</strong> on Profile
-        </Link>
-      </p>
+      <div className="text-center p-3">
+        <hr className="border-primary" />
+        <p>
+          {title}
+          <Link to="/profile/">
+            <br />
+            <strong>{author}</strong> on Profile
+          </Link>
+        </p>
+      </div>
     </div>
+    <style jsx>{`
+      .footer{
+        background-color: #eee;
+      }
+    `}</style>
   </div>
 )
 
