d8732652cd90cd5586fd07a3d8ff587f9c656281
azxca1731 <azxca1731@gmail.com>
2018-11-18 21:23:36 +0900

    more 버튼과 디스커스 들어가야지만 볼 수 있게

--- src/templates/Post/index.js
+++ src/templates/Post/index.js
@@ -6,7 +6,7 @@ import Img from 'gatsby-image'
 
 import Adsense from 'components/Adsense'
 import Footer from 'components/Footer'
-import Disqus from 'disqus-react';
+import Disqus from 'disqus-react'
 import './style.scss'
 
 const MY_URL = 'https://azxca1731.netlify.com'
@@ -25,12 +25,12 @@ const Post = ({ data, options }) => {
   const html = get(data, 'html')
   const isMore = isIndex && !!html.match('<!--more-->')
   const fixed = get(image, 'childImageSharp.fixed')
-  const disqusShortname = 'nogada-develop-blog';
+  const disqusShortname = 'nogada-develop-blog'
   const disqusConfig = {
-      url: `${MY_URL}/${path}`,
-      identifier: path,
-      title
-  };
+    url: `${MY_URL}/${path}`,
+    identifier: path,
+    title,
+  }
   return (
     <div className="article" key={path}>
       <div className="container">
@@ -58,7 +58,14 @@ const Post = ({ data, options }) => {
         />
         {isMore ? Button({ path, label: 'MORE', primary: true }) : ''}
         {getAd(isIndex, adsense)}
-        <Disqus.DiscussionEmbed shortname={disqusShortname} config={disqusConfig} />
+        {isIndex ? (
+          ''
+        ) : (
+          <Disqus.DiscussionEmbed
+            shortname={disqusShortname}
+            config={disqusConfig}
+          />
+        )}
       </div>
     </div>
   )
